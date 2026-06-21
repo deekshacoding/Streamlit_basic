153975ab0d601a841c7f5f35cde58ce7b3195192
deekshacoding <deeksha001222@gmail.com>
2026-06-21 16:22:22 +0000

    streamlit basic and summary

---
+++ streamlit_demo.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="streamlit demo",
+    layout="centered"
+
+)
+    
+
+st.title("welcome to streamlit")
+
+st.header("a simple and powerful way to build web apps in python")
+
+st.subheader("perfect for data science ,ML, and teaching")
+
